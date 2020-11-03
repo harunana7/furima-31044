94e8f12f7c94ec1783997ba8b4705ca1bc79667f
harunana7 <72502308+harunana7@users.noreply.github.com>
2020-11-04 00:04:37 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nicname, presence: true
+         validates :name, presence: true
+         validates :furigana, presence: true
+         validates :birth_date, presence: true
 end
