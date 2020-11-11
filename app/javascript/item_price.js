14450c18af814bd20640e2bb33ac5293a8ac2276
harunana7 <72502308+harunana7@users.noreply.github.com>
2020-11-11 20:49:44 +0900

    コメントの削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,6 +1,5 @@
 window.addEventListener('load', () => {
   console.log("OK");
-  // 金額を入力した数値をpriceInputという変数に格納する
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
