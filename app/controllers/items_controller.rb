85de55cca6ff2dea02c8359df4ced45d31511396
harunana7 <72502308+harunana7@users.noreply.github.com>
2020-11-15 15:36:26 +0900

    editアクションの条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-	before_action :authenticate_user!, only: [:new]
+	before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -22,9 +22,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
 	end
 	
-	def edit
-    @item = Item.find(params[:id])
-	end
+  def edit
+    @item =Item.find(params[:id])
+    redirect_to root_path if current_user.id != @item.user_id
+  end
 
   def update
     @item = Item.find(params[:id])
